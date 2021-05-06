071af206cc851f2a20c12be7bee4b146b2ffc508
Pi <p.cacheiro@qmul.ac.uk>
2021-05-06 15:58:40 +0100

    style edits to hpo obo to txt script

--- scripts/HPO_obo_to_txt.R
+++ scripts/HPO_obo_to_txt.R
@@ -1,147 +1,169 @@
-############################################################################################
-############################################################################################
+# script description ------------------------------------------------------
 
 ## Project: Ontologies
 ## Script: HPO_obo_to_txt.R
-## Purpose: import Human Phenotype Ontology in obo format and convert to plain text files
-## Author: Pilar Cacheiro
-## Date: 24/01/20020
-## HPO notes: format-version: 1.2; data-version: hp/releases/2019-11-08
+## Purpose: import Human Phenotype Ontology in obo format and convert it to
+## to plain text files
+## HPO notes: format-version: 1.2; hp/releases/2021-04-13
 
-#############################################################################################
-#############################################################################################
+# install / load libraries ------------------------------------------------
 
-library(ontologyIndex);library(tidyr);library(dplyr);library(Hmisc)
+if (!require("ontologyIndex")) install.packages("ontologyIndex")
+library("ontologyIndex")
 
-##############################################################################################
+if (!require("dplyr")) install.packages("dplyr")
+library("dplyr")
 
+if (!require("tidyr")) install.packages("tidyr")
+library("tidyr")
+
+if (!require("Hmisc")) install.packages("Hmisc")
+library("Hmisc")
+
+# get ontology ------------------------------------------------------------
 
 hpo <- get_ontology("http://purl.obolibrary.org/obo/hp.obo")
 
 ## hpo ids and descriptions
 
-hpo.description <- data.frame(hpo.term=as.character(row.names(as.data.frame(hpo[[2]]))),
-                             hpo.description=as.character(as.data.frame(hpo[[2]])[,1]),stringsAsFactors = F)
+hpo_description <- data.frame(
+  hpo_term = as.character(row.names(as.data.frame(hpo[[2]]))),
+  hpo_description = as.character(as.data.frame(hpo[[2]])[, 1]), 
+  stringsAsFactors = F)
 
-## parents, children and all the ancestros for a given hpoO term as a list
+## parents, children and all the ancetors for a given hpo term as a list
 
 parents <- hpo[[3]]
 children <-  hpo[[4]]
 ancestors <- hpo[[5]]
 
 
-## set of functions to convert these lists to data frame format:
+# set of functions to convert these lists to data frames ------------------
 
 
-#### get parental nodes ########################################################################
+## get parental nodes 
 
-get.parent.nodes <- function(parents){
+get_parent_nodes <- function(parents) {
   
-  hpo.term <- list()
-  hpo.parents <-list()
+  hpo_term <- list()
+  hpo_parents <-list()
   
   for(i in 1:length(names(parents))){
     
-    hpo.term [[i]] <- names(parents)[i]
-    hpo.parents[[i]] <- paste(parents[[i]],collapse=",")
+    hpo_term [[i]] <- names(parents)[i]
+    hpo_parents[[i]] <- paste(parents[[i]], collapse=",")
   }
   
-  hpo.parents <- data.frame(hpo.term = do.call(rbind,hpo.term),hpo.parents = do.call(rbind,hpo.parents),stringsAsFactors=F) %>%
-    mutate(hpo.parents = strsplit(as.character(hpo.parents),",")) %>%
-    unnest(hpo.parents) %>%
-    filter(hpo.term != hpo.parents)
+  hpo_parents <- data.frame(
+    hpo_term = do.call(rbind,hpo_term), 
+    hpo_parents = do.call(rbind, hpo_parents), stringsAsFactors=F) %>%
+    mutate(hpo_parents = strsplit(as.character(hpo_parents), ",")) %>%
+    unnest(hpo_parents) %>%
+    filter(hpo_term != hpo_parents)
   
-  return(hpo.parents)
+  return(hpo_parents)
   
 }
 
 
-#### get children nodes ##############################################################################
+## get children nodes 
 
-get.children.nodes <- function(children){
+get_children_nodes <- function(children) {
   
-  hpo.term <- list()
-  hpo.children <- list()
+  hpo_term <- list()
+  hpo_children <- list()
   
   for(i in 1:length(names(children))){
     
-    hpo.term [[i]] <- names(children)[i]
-    hpo.children[[i]] <- paste(children[[i]],collapse=",")
+    hpo_term [[i]] <- names(children)[i]
+    hpo_children[[i]] <- paste(children[[i]], collapse=",")
   }
   
-  hpo.children <-  data.frame(hpo.term = do.call(rbind,hpo.term),hpo.children= do.call(rbind,hpo.children),stringsAsFactors=F) %>%
-    mutate(hpo.children = strsplit(as.character(hpo.children),",")) %>%
-    unnest(hpo.children) %>%
-    filter(hpo.term != hpo.children)
+  hpo_children <-  data.frame(
+    hpo_term = do.call(rbind, hpo_term),
+    hpo_children = do.call(rbind, hpo_children), stringsAsFactors=F) %>%
+    mutate(hpo_children = strsplit(as.character(hpo_children), ",")) %>%
+    unnest(hpo_children) %>%
+    filter(hpo_term != hpo_children)
   
-  return(hpo.children)
+  return(hpo_children)
 }
 
 
-#### get all the ancestors/ top levels ##################################################################
+## get all the ancestors/ top levels
 
-get.ancestor.nodes <- function(ancestors){
+get_ancestor_nodes <- function(ancestors) {
   
-  hpo.term <- list()
-  hpo.ancestors <- list()
+  hpo_term <- list()
+  hpo_ancestors <- list()
   
-  for(i in 1:length(names(ancestors))){
+  for(i in 1:length(names(ancestors))) {
     
-    hpo.term [[i]] <- names(ancestors)[i]
-    hpo.ancestors[[i]] <- paste(ancestors[[i]],collapse=",")
+    hpo_term [[i]] <- names(ancestors)[i]
+    hpo_ancestors[[i]] <- paste(ancestors[[i]], collapse=",")
   }
   
-  hpo.ancestors <- data.frame(hpo.term = do.call(rbind,hpo.term),hpo.ancestors = do.call(rbind,hpo.ancestors),stringsAsFactors=F) %>%
-    mutate(hpo.ancestors = strsplit(as.character(hpo.ancestors),",")) %>%
-    unnest(hpo.ancestors) %>%
-    filter(hpo.term != hpo.ancestors )
+  hpo_ancestors <- data.frame(
+    hpo_term = do.call(rbind, hpo_term),
+    hpo_ancestors = do.call(rbind, hpo_ancestors),stringsAsFactors=F) %>%
+    mutate(hpo_ancestors = strsplit(as.character(hpo_ancestors), ",")) %>%
+    unnest(hpo_ancestors) %>%
+    filter(hpo_term != hpo_ancestors )
   
-  return(hpo.ancestors)
+  return(hpo_ancestors)
 }
 
-### get top levels #########################################################################################
+## get top levels of the ontology (physiological systems)
 
-hpo.toplevels <- get.parent.nodes(parents) %>%
-  filter(hpo.parents=="HP:0000001") %>%
-  left_join(hpo.description,by="hpo.term") %>%
-  select(hpo.term,hpo.description)
+hpo_toplevels <- get_parent_nodes(parents) %>%
+  filter(hpo_parents == "HP:0000001") %>%
+  left_join(hpo_description,by = "hpo_term") %>%
+  select(hpo_term, hpo_description)
 
-hpo.toplevels.phenotypic.abnormalities.only <- get.parent.nodes(parents) %>%
-  filter(hpo.parents=="HP:0000118") %>%
-  left_join(hpo.description,by="hpo.term") %>%
-  select(hpo.term,hpo.description)
-
-##############################################################################################################
-##############################################################################################################
-##############################################################################################################
+hpo_toplevels_phenotypic_abnormalities_only <- get_parent_nodes(parents) %>%
+  filter(hpo_parents == "HP:0000118") %>%
+  left_join(hpo_description,by = "hpo_term") %>%
+  select(hpo_term,hpo_description)
 
 
-hpo.parental.nodes <- get.parent.nodes(parents)
-
-hpo.children.nodes <- get.children.nodes(children)
-
-hpo.ancestor.nodes <- get.ancestor.nodes(ancestors)
+# apply functions ---------------------------------------------------------
 
 
-#############################################################################################################
+hpo_parental_nodes <- get_parent_nodes(parents)
 
-## export files
+hpo_children_nodes <- get_children_nodes(children)
 
-hpo.dir <- "../hpo/"
-
-files.to.export <- list(hpo.description,hpo.parental.nodes,hpo.children.nodes,hpo.ancestor.nodes,
-                       hpo.toplevels,hpo.toplevels.phenotypic.abnormalities.only)
-
-names(files.to.export) <- Cs(hpo.description,hpo.parental.nodes,hpo.children.nodes,hpo.ancestor.nodes,
-                            hpo.toplevels,hpo.toplevels.phenotypic.abnormalities.only)
+hpo_ancestor_nodes <- get_ancestor_nodes(ancestors)
 
 
-for (i in 1:length(files.to.export)){
+
+# export files ------------------------------------------------------------
+
+
+hpo_dir <- "./data/"
+
+
+files_to_export <- list(hpo_description, 
+                        hpo_parental_nodes,
+                        hpo_children_nodes,
+                        hpo_ancestor_nodes,
+                        hpo_toplevels,
+                        hpo_toplevels_phenotypic_abnormalities_only)
+
+names(files_to_export) <- Cs(hpo_description,
+                             hpo_parental_nodes,
+                             hpo_children_nodes,
+                             hpo_ancestor_nodes,
+                             hpo_toplevels,
+                             hpo_toplevels_phenotypic_abnormalities_only)
+
+
+for (i in 1:length(files_to_export)){
   
-  write.table(files.to.export[[i]],paste0(hpo.dir,names(files.to.export)[i],".txt"), quote = F,sep="\t",row.names = FALSE)
+  write.table(files_to_export[[i]],
+              paste0(hpo_dir, names(files_to_export)[i], ".txt"), 
+              quote = F, sep = "\t", row.names = FALSE)
   
 }
 
-#############################################################################################################
-#############################################################################################################
 
